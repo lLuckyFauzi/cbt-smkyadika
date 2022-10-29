317b0336c5c2d1704cab021d1650d65c7700ac14
Lucky <Luckyfauzi2004@gmail.com>
2022-10-29 17:33:10 +0700

    refactoring_to_src

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,38 +1,67 @@
-import { Button } from 'antd'
-import type { NextPage } from 'next'
-import { useState } from 'react'
-import Text from '../../component/Typography/Text'
+import { Button } from "antd";
+import type { NextPage } from "next";
+import { useState } from "react";
+import Text from "../../components/Typography/Text";
 
 const Home: NextPage = () => {
-  const [ count, setCount ] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      minHeight: '100vh'
-    }}>
-      <div style={{
-        textAlign: 'center'
-      }}>
-        <Text size={55} color="main" bold style={{padding: '5px', borderRadius: '8px'}}>Kokys</Text>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <Text
+          size={55}
+          color="main"
+          bold
+          style={{ padding: "5px", borderRadius: "8px" }}
+        >
+          Kokys
+        </Text>
         <br />
-        <Text size={20} color='lightGray' style={{ padding: '5px', backgroundColor: 'white', borderRadius: '8px'}}>Happy coding with Koky's Template! ;)</Text>
+        <Text
+          size={20}
+          color="lightGray"
+          style={{
+            padding: "5px",
+            backgroundColor: "white",
+            borderRadius: "8px",
+          }}
+        >
+          Happy coding with Koky's Template! ;)
+        </Text>
         <br />
-        <div style={{marginTop: '20px'}}>
-          <Text size={35} color='main'>{count}</Text>
+        <div style={{ marginTop: "20px" }}>
+          <Text size={35} color="main">
+            {count}
+          </Text>
         </div>
         <br />
-        <Button type='primary' style={{
-          marginTop: '20px',
-          borderRadius: '5px'
-        }} onClick={() => {
-          setCount(prev => prev + 1)
-        }}>Add</Button>
+        <Button
+          type="primary"
+          style={{
+            marginTop: "20px",
+            borderRadius: "5px",
+          }}
+          onClick={() => {
+            setCount((prev) => prev + 1);
+          }}
+        >
+          Add
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
