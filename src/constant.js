216c8b0e83cb95618532013b2008d9698f80fe2f
Lucky <Luckyfauzi2004@gmail.com>
2022-11-16 02:22:00 +0700

    delete_function

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
-// const local_replit = "https://vanilla-cbt.smkyadikasrg.repl.co/v1/";
-const local_replit = "http://localhost:8000/v1/";
+const local_replit = "https://vanilla-cbt.smkyadikasrg.repl.co/v1/";
+// const local_replit = "http://localhost:8000/v1/";
 
 module.exports = {
   LOCAL_API: local_replit,
